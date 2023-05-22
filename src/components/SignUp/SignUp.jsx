a721b6a69c932e30e196f13f1c6a547fa1a0ab8b
mayank0801 <mayanksingh0801@gmail.com>
2023-05-22 16:08:58 +0530

    feat:Added Sign up Feature

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,8 +1,21 @@
 import { Link } from "react-router-dom";
 import { MdNavigateNext } from "react-icons/md";
 import "./SignUp.css";
+import { useContext, useState } from "react";
+
+import { AuthContext } from "../../context/AuthContext/AuthContext";
 
 export default function SignUp() {
+
+
+    const {signupHandler}=useContext(AuthContext);
+    const [userInfo,setUserInfo]=useState({firstName:"",lastName:"",email:"",password:""});
+    const formHandler=(e)=>{
+        const name=e.target.name;
+        const value=e.target.value
+       return setUserInfo({...userInfo,[name]:value})
+    }
+    // console.log(userInfo,"formHandler")
   return (
     <section className="auth-container">
       <div className="auth-main-container">
@@ -13,25 +26,25 @@ export default function SignUp() {
           <div className="user-info">
             <div className="user-name">
               <label>First Name: </label>
-              <input type="text" placeholder="Test" />
+              <input type="text" name="firstName" placeholder="Test" onChange={(e)=>formHandler(e)}/>
             </div>
             <div className="user-name">
               <label>Last Name: </label>
-              <input type="text" placeholder="Test" />
+              <input type="text" name="lastName" placeholder="Test" onChange={(e)=>formHandler(e)}/>
             </div>
           </div>
           <div className="input-container">
             <label htmlFor="email">Email:</label>
-            <input type="email" placeholder="test@gmail.com" />
+            <input type="email" name="email" placeholder="test@gmail.com" onChange={(e)=>formHandler(e)} />
           </div>
           <div className="input-container">
             <label htmlFor="password">Password:</label>
-            <input type="password" placeholder="*****" />
+            <input type="password" name="password" placeholder="*****" onChange={(e)=>formHandler(e)}/>
           </div>
         </main>
         <footer>
           <div className="btn-primary">
-            <span>Create New Account </span>
+            <span onClick={()=>signupHandler(userInfo)}>Create New Account </span>
           </div>
           <div className="auth-navigater">
             <Link className="link-primary" to="/login">
--- src/context/AuthContext/AuthContext.js
+++ src/context/AuthContext/AuthContext.js
@@ -19,7 +19,22 @@ export default function AuthContextProvider({children}){
       };
 
 
+      const signupHandler = async (userInfo) => {
+        console.log("Signup Hnadler with",userInfo)
+        try {
+          const response = await axios.post(`/api/auth/signup`, {
+           ...userInfo
+          });
+          console.log(response);
+          // saving the encodedToken in the localStorage
+          localStorage.setItem("token", response.data.encodedToken);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+
     return(
-        <AuthContext.Provider value={{loginHandler}}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={{loginHandler,signupHandler}}>{children}</AuthContext.Provider>
     )
 }
